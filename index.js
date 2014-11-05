d87e23a00654874edb3bffd29adf14c1d1ce8203
Damon Oehlman <damon.oehlman@gmail.com>
2014-11-05 14:09:06 +1100

    Complete implementation

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var curry = require('curry');
+var KEYS_MAXWIDTH = ['maxWidth', 'width.max'];
+var KEYS_MINWIDTH = ['minWidth', 'width.min'];
+var KEYS_MAXHEIGHT = ['maxHeight', 'height.max'];
+var KEYS_MINHEIGHT = ['minHeight', 'height.min'];
+var KEYS_MAXFPS = ['maxFrameRate', 'frameRate.max'];
+var KEYS_MINFPS = ['minFrameRate', 'frameRate.min'];
 
 /**
   # rtc-constraints
@@ -8,18 +14,37 @@ var curry = require('curry');
   ## Reference
 **/
 
-var tweak = module.exports = function(targets, mediaType) {
+var tweak = module.exports = function(targets, opts) {
+  var mediaType = (opts || {}).mediaType || 'video';
 
   function createSetter(target) {
     var keys = target.split('.');
     var lastIdx = keys.length - 1;
 
-    return function(data, value) {
-      keys.forEach(function(key, idx) {
-        if ((idx < lastIdx) && (! data[key])) {
-          data = data[key] = {};
+    return function setValue(data, value) {
+      var existingSection;
+
+      if (Array.isArray(data)) {
+        existingSection = data.filter(function(item) {
+          return item[keys[0]];
+        })[0];
+
+        if (existingSection) {
+          return setValue(existingSection, value);
         }
-        else if (idx === lastIdx) {
+
+        return setValue(data[data.length] = {}, value);
+      }
+
+      keys.forEach(function(key, idx) {
+        if (idx < lastIdx) {
+          if (! data[key]) {
+            data[key] = {};
+          }
+
+          data = data[key];
+        }
+        else {
           data[key] = value;
         }
       });
@@ -41,18 +66,13 @@ var tweak = module.exports = function(targets, mediaType) {
     }
 
     targets.forEach(function(updater) {
-      if (Array.isArray(sectionData)) {
-        updater(sectionData[sectionData.length] = {}, value);
-        return;
-      }
-
       updater(sectionData, value);
     });
 
     return constraints;
   });
 
-  var tweaker = applyTweak('mandatory');
+  var tweaker = applyTweak((opts || {}).section || 'optional');
 
   // ensure targets is an array
   targets = [].concat(targets || []).map(createSetter);
@@ -62,8 +82,19 @@ var tweak = module.exports = function(targets, mediaType) {
 
   // add the optional helper to allow optional modifications
   tweaker.optional = tweaker.opt = applyTweak('optional');
+  tweaker.mandatory = applyTweak('mandatory');
 
   return tweaker;
 };
 
-tweak.fps = tweak.maxfps = tweak(['frameRate.max']);
+tweak.maxwidth = tweak(KEYS_MAXWIDTH);
+tweak.minwidth = tweak(KEYS_MINWIDTH);
+tweak.width = tweak.w = tweak(KEYS_MINWIDTH.concat(KEYS_MAXWIDTH));
+
+tweak.maxheight = tweak(KEYS_MAXHEIGHT);
+tweak.minheight = tweak(KEYS_MINHEIGHT);
+tweak.height = tweak.h = tweak(KEYS_MINHEIGHT.concat(KEYS_MAXHEIGHT));
+
+tweak.maxfps = tweak(KEYS_MAXFPS);
+tweak.minfps = tweak(KEYS_MINFPS);
+tweak.fps = tweak(KEYS_MINFPS.concat(KEYS_MAXFPS));
